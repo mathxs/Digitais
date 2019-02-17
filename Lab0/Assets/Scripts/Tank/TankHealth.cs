6e1e64dc70f57420c425f36304da91366bfc69dd
mathxs <38170401+mathxs@users.noreply.github.com>
2019-02-17 20:54:31 -0300

    update bug
    vida consertada, porem as cores não estao refletindo a logica

--- Lab0/Assets/Scripts/Tank/TankHealth.cs
+++ Lab0/Assets/Scripts/Tank/TankHealth.cs
@@ -37,13 +37,10 @@ public class TankHealth : MonoBehaviour
 
     public void TakeDamage(float amount)
     {
-        // Reduce current health by the amount of damage done.
         m_CurrentHealth -= amount;
 
-        // Change the UI elements appropriately.
         SetHealthUI();
 
-        // If the current health is at or below zero and it has not yet been registered, call OnDeath.
         if (m_CurrentHealth <= 0f && !m_Dead)
         {
             OnDeath();
@@ -53,30 +50,22 @@ public class TankHealth : MonoBehaviour
 
     private void SetHealthUI()
     {
-        // Set the slider's value appropriately.
         m_Slider.value = m_CurrentHealth;
-
-        // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
         m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
     }
 
 
     private void OnDeath()
     {
-        // Set the flag so that this function is only called once.
         m_Dead = true;
 
-        // Move the instantiated explosion prefab to the tank's position and turn it on.
         m_ExplosionParticles.transform.position = transform.position;
         m_ExplosionParticles.gameObject.SetActive(true);
 
-        // Play the particle system of the tank exploding.
         m_ExplosionParticles.Play();
 
-        // Play the tank explosion sound effect.
         m_ExplosionAudio.Play();
 
-        // Turn the tank off.
         gameObject.SetActive(false);
     }
 }
